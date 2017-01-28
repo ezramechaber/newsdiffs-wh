52d6c3c49ab7d33e2b3e4f89e1b96d983fd9c87e
Albert J. Wong <awong.dev@gmail.com>
2017-01-28 00:36:57 -0800

    More sql ugliness.

--- website/frontend/views.py
+++ website/frontend/views.py
@@ -71,7 +71,7 @@ def get_articles(source=None, distance=0):
      (SELECT "Articles".id as article_id, MAX(T3.date) AS age, COUNT(T3.id) AS num_vs
       FROM "Articles" LEFT OUTER JOIN version T3 ON ("Articles".id = T3.article_id)
       WHERE (T3.boring=FALSE) GROUP BY "Articles".id
-      HAVING (age > %s  AND age < %s  AND num_vs > 1 )) T, "Articles"
+      HAVING (T3.date > %s  AND T3.date < %s  AND T3.id > 1 )) T, "Articles"
     WHERE (version.article_id = "Articles".id) and
           (version.article_id = T.article_id) and
           NOT version.boring
